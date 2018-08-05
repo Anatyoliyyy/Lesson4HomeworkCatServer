0074a2e368b40e57d19411b5ad9de6a25526ffd0
Anatyoliyyy <anatoliyyym@ukr.net>
2018-08-05 13:37:50 +0300

    Initial commit

---
+++ src/main/java/ua/kiev/prog/UserList.java
@@ -0,0 +1,21 @@
+package ua.kiev.prog;
+
+import java.io.*;
+import java.util.*;
+
+public class UserList {
+    private static final UserList userList = new UserList();
+    private final List<User> list = new LinkedList<User>();
+
+    public List<User> getList() {
+        return list;
+    }
+
+    public static UserList getInstance() {
+        return userList;
+    }
+
+    public synchronized void add(User user) {
+        list.add(user);
+    }
+}
